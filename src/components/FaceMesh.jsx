3b3596e07d403befba12517aa30135ee7271b065
Jorrmungandr <edgar@lunary.ai>
2025-02-12 11:32:46 -0300

    feat: change video height

--- src/components/FaceMesh.jsx
+++ src/components/FaceMesh.jsx
@@ -145,8 +145,8 @@ export const FaceMeshMirror = ({ windowWidth, windowHeight }) => {
           position: 'absolute',
           top: 0,
           left: 0,
-          width: '100vw',
-          height: '100vh',
+          width: '100%',
+          height: '100%',
           objectFit: 'contain',
         }}
         autoPlay
